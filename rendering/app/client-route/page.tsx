2aea6072a310bc5fff24b8493fb37223073111d5
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2024-08-27 04:42:35 +0200

    install real-slick carousel

--- rendering/app/client-route/page.tsx
+++ rendering/app/client-route/page.tsx
@@ -1,14 +1,38 @@
 "use client";
-import serverSideFunction from "@/utils/server-utils";
+
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const ClientRoutePage = () => {
   console.log("Client route rendered");
 
-  const result = serverSideFunction();
+  const settings = {
+    dots: true,
+  };
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Client Route Page</h1>
-      <p>{result}</p>
+    <div className="h-screen">
+      <div
+        className="image-slider-container 
+   "
+      >
+        <h1 className="font-bold text-3xl mb-8">Client Route Page</h1>
+        <Slider {...settings}>
+          <div>
+            <img src="http://picsum.photos/400/200" />
+          </div>
+          <div>
+            <img src="http://picsum.photos/400/200" />
+          </div>
+          <div>
+            <img src="http://picsum.photos/400/200" />
+          </div>
+          <div>
+            <img src="http://picsum.photos/400/200" />
+          </div>
+        </Slider>
+      </div>{" "}
     </div>
   );
 };
--- rendering/app/server-route/page.tsx
+++ rendering/app/server-route/page.tsx
@@ -1,13 +1,20 @@
 import "server-only";
 import serverSideFunction from "@/utils/server-utils";
+import ImageSlider from "@/components/ImageSlider";
 
 function ServerRoutePage() {
   console.log("Server route rendered");
   const result = serverSideFunction();
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Server Route Page</h1>
-      <p>{result}</p>
+    <div
+      className="h-screen
+    "
+    >
+      <div className="image-slider-container ">
+        <h1 className="text-3xl font-bold">Server Route Page</h1>
+        <p>{result}</p>
+        <ImageSlider />
+      </div>
     </div>
   );
 }
