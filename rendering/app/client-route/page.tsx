1ddc49bb20c7419abd97b418fc97efdebff27db4
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2024-08-28 14:47:30 +0200

    delete server only

--- rendering/app/client-route/page.tsx
+++ rendering/app/client-route/page.tsx
@@ -6,12 +6,10 @@ import { useTheme } from "@/components/theme-provider";
 
 const ClientRoutePage = () => {
   const theme = useTheme();
-  const result = clientSideFunction();
+
   return (
     <div className="h-screen ">
-      <h1 className="text-3xl font-bold text-blue-600">
-        Client route {result}{" "}
-      </h1>
+      <h1 className="text-3xl font-bold text-blue-600">Client route</h1>
     </div>
   );
 };
--- rendering/utils/client-utils.ts
+++
@@ -1,7 +0,0 @@
-const clientSideFunction = () => {
-  console.log(`Use window object,
-    localstorage`);
-  return "client result";
-};
-
-export default clientSideFunction;
